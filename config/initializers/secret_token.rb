a005868b17362b0df5ed15ac9652c07b4018c427
Blake Rego <blake.rego@gmail.com>
2013-09-30 11:49:27 +0700

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HappilyAdrift::Application.config.secret_key_base = '1529b172625ca42dff452d4346b9b9124852159a4d9c7f1f8e7b333b4d0631a87dda5cfad96df2d84643d78d911ac5bae364ae7e417eb20fcd76ac1ee302b45b'
