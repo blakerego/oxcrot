aedcf76d78a14f41bcc37477db96ba1e82c4ae2e
Blake Rego <blake.rego@gmail.com>
2013-09-30 17:37:26 +0700

    fixing prod assets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ HappilyAdrift::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -71,8 +71,6 @@ HappilyAdrift::Application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-
-  config.assets.initialize_on_precompile = false
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
