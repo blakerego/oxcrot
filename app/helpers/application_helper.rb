124cb9ba4baa03100c4da0198993bdbe4feba663
Blake Rego <blake.rego@gmail.com>
2013-10-06 00:12:41 +0700

    adding some styling to the grid layout on the home page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
     doc = Nokogiri::HTML( post['content'] )
     return doc.css('img').first['src']
   end
+
+  def trim_excerpt(post, length=150)
+    doc = Nokogiri::HTML( post['excerpt'] )
+    return truncate(doc.css('p').first.text, :length => length)
+  end
+
+  def post_date(post)
+    raw_date = post['date']
+    return DateTime.parse(raw_date).strftime("%B %d, %Y")
+  end
 end
