ab4114f0d22f158935b5c1705e7220275d1aeb69
Blake Rego <blake.rego@gmail.com>
2013-10-09 23:11:30 +0700

    adding model validation for comment name, email and content.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ActiveRecord::Base
-
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :content, presence: true
   def self.comments_for_post_id(post_id)
     Comment.where(:post_id => post_id).order('created_at DESC')
   end
