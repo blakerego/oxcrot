55033fa1db2b4ef9556d19e29fc44c4c1f5cefb0
Blake Rego <blake.rego@gmail.com>
2013-10-09 16:30:55 +0700

    adding infinite scrolling to main page so older posts load automatically.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,21 @@
 class PostsController < ApplicationController
 
   def index
-    data = query_api_via_get("#{site_url}/posts")
+    if params[:page].present?
+      @page = params[:page].to_i
+    else
+      @page = 1
+    end
+    @next_page = @page + 1
+    per_page = 8
+
+    data = query_api_via_get("#{site_url}/posts?page=#{@page.to_s}&number=#{per_page}")
     parsed = JSON.parse(data.body)
     @posts = parsed['posts']
+
+    if request.xhr?
+      render :partial => 'posts_preview', :layout => false, :locals => {:posts => @posts}
+    end
   end
 
   def single_posts
